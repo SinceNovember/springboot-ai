2a34893e3f415e7284fb0b22b04415cb2e648cc3
SinceNovember <83428190@qq.com>
2026-03-30 22:33:44 +0800

    feat: rag.

---
+++ ai-rag-knowledge/dev-api/src/main/java/com/simple/IAiService.java
@@ -0,0 +1,14 @@
+package com.simple;
+
+import org.springframework.ai.chat.model.ChatResponse;
+import reactor.core.publisher.Flux;
+
+public interface IAiService {
+
+    ChatResponse generate(String model, String message);
+
+    Flux<ChatResponse> generateStream(String model, String message);
+
+    Flux<ChatResponse> generateStreamRag(String model, String ragTag, String message);
+
+}
---
+++ ai-rag-knowledge/dev-app/src/main/java/com/simple/config/OpenaiConfig.java
@@ -0,0 +1,41 @@
+package com.simple.config;
+
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+public class OpenaiConfig {
+
+    @Bean
+    public OpenAiApi openAiApi(@Value("${spring.ai.openai.base-url}") String baseUrl, @Value("${spring.ai.openai.api-key}") String apikey) {
+        return OpenAiApi.builder().baseUrl(baseUrl).apiKey(apikey).build();
+    }
+
+    @Bean
+    public TokenTextSplitter tokenTextSplitter() {
+        return new TokenTextSplitter();
+    }
+
+    @Bean
+    public OpenAiEmbeddingModel embeddingModel(OpenAiApi openAiApi) {
+        return new OpenAiEmbeddingModel(openAiApi);
+    }
+
+    @Bean
+    public SimpleVectorStore vectorStore(OpenAiEmbeddingModel embeddingModel) {
+        return SimpleVectorStore.builder(embeddingModel).build();
+    }
+
+    @Bean
+    public PgVectorStore pgVectorStore(OpenAiEmbeddingModel embeddingModel, JdbcTemplate jdbcTemplate) {
+        return PgVectorStore.builder(jdbcTemplate, embeddingModel).build();
+    }
+
+}
