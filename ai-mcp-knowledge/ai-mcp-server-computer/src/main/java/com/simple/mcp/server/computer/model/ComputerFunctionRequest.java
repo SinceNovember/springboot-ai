92c896555489ccbf389ca59e6375767c04e33336
SinceNovember <83428190@qq.com>
2026-07-21 23:15:02 +0800

    feat: agent.

---
+++ ai-mcp-knowledge/ai-mcp-server-computer/src/main/java/com/simple/mcp/server/computer/model/ComputerFunctionRequest.java
@@ -0,0 +1,17 @@
+package com.simple.mcp.server.computer.model;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ComputerFunctionRequest {
+
+    @JsonProperty(required = true, value = "computer")
+    @JsonPropertyDescription("电脑名称")
+    private String computer;
+
+}
